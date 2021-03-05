97e7647a12f9d52d3e1fdf2eb83f3a75fe71d1f5
okelloEnos <enosokello@gmail.com>
2021-03-05 21:52:13 +0300

    loading location time

---
+++ lib/Service/World_time.dart
@@ -0,0 +1,48 @@
+
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+class WorldTime{
+  String location;  // place location
+  String flag;  // path to location flag
+  String time;  // location time
+  String url;   // url endpoint
+
+  WorldTime({this.location, this.flag, this.url});
+
+ Future <void> getTime() async {
+   
+   try {
+     // make the request
+     // Response response = await get("http://worldtimeapi.org/api/timezone/Africa/Nairobi");
+     // Response responses = await get(Uri.parse('http://worldtimeapi.org/api/timezone/Africa/Nairobi'));
+     // Map data = jsonDecode(responses);
+     var uri = Uri.http('worldtimeapi.org', '/api/timezone/$url');
+     var response = await get(uri);
+     var jsonResponse = jsonDecode(response.body);
+     var abbr = jsonResponse['abbreviation'];
+
+     // get properties
+     String datetime = jsonResponse['datetime'];
+     String offset = jsonResponse['utc_offset'].toString().substring(1, 3);
+
+     // print('Time abbr : $abbr');
+     print('datetime : $datetime');
+     print('Time offset : $offset');
+
+     //  create a date time object
+     DateTime now = DateTime.parse(datetime);
+     now = now.add(Duration(hours: int.parse(offset)));
+     print('Current time : $now');
+
+     //set time property
+     time = now.toString();
+   }
+   
+   catch(e){
+     print('error: $e');
+     time = 'Unable to Obtain Time for the Location';
+   }
+  }
+}
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,8 +1,6 @@
-import 'dart:convert';
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:flutter/widgets.dart';
+import 'package:world_time/Service/World_time.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -11,42 +9,35 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getTime() async {
-    //make the request
-    // Response response = await get("http://worldtimeapi.org/api/timezone/Africa/Nairobi");
-    // Response responses = await get(Uri.parse('http://worldtimeapi.org/api/timezone/Africa/Nairobi'));
-    // Map data = jsonDecode(responses);
-    var uri = Uri.http('worldtimeapi.org', '/api/timezone/Africa/Nairobi');
-    var response = await get(uri);
-    var jsonResponse = jsonDecode(response.body);
-    var abbr = jsonResponse['abbreviation'];
+  String time = 'Loading';
 
-    // get properties
-    String datetime = jsonResponse['datetime'];
-    String offset = jsonResponse['utc_offset'].toString().substring(1,3);
+  void setUpWorldTime() async{
+    WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.jpg', url: 'Europe/Berlin');
+    await instance.getTime();
+    print(instance.time);
 
-    // print('Time abbr : $abbr');
-    print('datetime : $datetime');
-    print('Time offset : $offset');
-
-  //  create a date time object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    print('Current time : $now');
-
+    setState(() {
+      time = instance.time;
+    });
   }
+
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    getTime();
+    setUpWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text("Loading Screen")),
+      body: SafeArea(
+          child: Padding(
+              padding: EdgeInsets.all(50.0),
+          child: Text(time),
+          ),
+      ),
     );
   }
 }
